d42e0b623a66f7ca439326a78b5aa38fd833bd68
Chemineau Louis <cheminl1@cs-159.org.aalto.fi>
2017-07-11 15:54:51 +0300

    Update

--- template.py
+++ template.py
@@ -1,21 +1,26 @@
 import gym
+from gym.wrappers.monitoring import Monitor
 from random import randint
+import time
 import math
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-def learn(episodeCount):
+
+def learn(episodeCount, stepsHistory, decayHistory, i_learn):
 	for i_episode in range(episodeCount):  # Start an episode
 		obs = env.reset()
+		# Reset reward count and states/actions for this episode
 		# Compute the decay of the exploration
-		decayX = 0.02
-		decayY = 20
+		decayX = 0.5
+		decayY = 50
 		decay = max(-i_episode*decayX+decayY, 10/(i_episode+1))
-		doEpisodeMC(obs, decay, i_episode)
+		decayHistory[i_episode] = decay
+		#doEpisodeMC(obs, decay, i_episode, i_learn)
+		doEpisodeTD(obs, decay, i_episode, i_learn)
 
 
-def doEpisodeMC(obs, decay, i_episode):
-	# Reset reward count and states/actions for this episode
+def doEpisodeMC(obs, decay, i_episode, i_learn):
 	episodeStatesActions = []
 	totalRewards = 0
 	for t in range(200):
@@ -25,9 +30,27 @@ def doEpisodeMC(obs, decay, i_episode):
 		obs, reward, done, _ = env.step(action)  # Apply the action
 		totalRewards += reward  # Update total reward for this episode
 		if done:  # Episode is over
-			stepsHistory[i_episode] = t
+			stepsHistory[i_episode] = (stepsHistory[i_episode]*i_learn + t+1)/(i_learn+1)
 			for i, state_action in enumerate(episodeStatesActions):  # Update value for chosen actions
-				updatePolicyMC(state_action['state'], state_action['action'], totalRewards-i)
+				updatePolicyMCa(state_action['state'], state_action['action'], totalReward-i)
+			break
+
+
+def doEpisodeTD(obs, decay, i_episode, i_learn):
+	episodeStatesActions = []
+	lastState  = None
+	lastAction = None
+	for t in range(200):
+		state = getState(obs)  # Get the state
+		action = policy(state, decay)  # Get the action
+		episodeStatesActions.append({'state': state, 'action': action})  # Save state and action to episodeStatesActions
+		if t > 0:
+			updatePolicyTD(lastState, lastAction, state, action, reward)
+		obs, reward, done, _ = env.step(action)  # Apply the action
+		lastState  = state
+		lastAction = action
+		if done:  # Episode is over
+			stepsHistory[i_episode] = (stepsHistory[i_episode]*i_learn + t+1)/(i_learn+1)
 			break
 
 
@@ -40,14 +63,31 @@ def updatePolicyMC(state, action, G):
 	a['count'] += 1
 
 
+def updatePolicyMCa(state, action, G):
+	a = history[state][action]
+	a['value'] = a['value'] + 0.1 * (G - a['value'])
+
+
+# @param state <string> the state to update
+# @param action <int> the action to update
+# @param <int> the reward
+def updatePolicyTD(state1, action1, state2, action2, reward):
+	a1 = history[state1][action1]
+	a2 = history[state2][action2]
+	a1['value'] = a1['value'] + 0.1 * (reward + 1 * a2['value'] - a1['value'])
+	a1['count'] += 1
+
+
 # @param obs <[float]> the observation to convert into a state
 # @return the state associated to the observation_space
 # If the set of observations where never met, create the state
 # The function reduces the number of possible states
 def getState(obs):
 	state = ''
-	for o in obs:
-		state += str(math.floor(o))
+	state += str(math.floor(obs[0]))
+	state += str(math.floor(obs[1]))
+	state += str(math.floor(obs[2]))
+	state += str(math.floor(obs[3]))
 	return state
 
 
@@ -70,21 +110,23 @@ def policy(state, decay):
 		if stateValues[minCountAction]['count'] > stateValues[action]['count']:
 			minCountAction = action
 	# Computing the decay of the exploration
-	if randint(0, 100) < decay:
+	if randint(0, 100) < 10:
 		return minCountAction
 	else:
 		return maxValueAction
 
 
-env = gym.make('CartPole-v0')
-nbEpisodes = 2000
+nbEpisodes = 5000
 stepsHistory = [0]*nbEpisodes
-
-history = {}  # 'state' ==> [{'count': int, 'value': float}]
-
-learn(nbEpisodes)
-
+env = gym.make('CartPole-v0')
+#env = Monitor(env, 'tmp/cart-pole', force=True)
+for i in range(6):
+	print i
+	history = {}  # 'state' ==> [{'count': int, 'value': float}]
+	decayHistory = [0]*nbEpisodes
+	learn(nbEpisodes, stepsHistory, decayHistory, i)
 env.close()
-plt.plot(range(nbEpisodes), stepsHistory, range(nbEpisodes), [195]*nbEpisodes)
+#gym.upload('tmp/cart-pole', api_key='sk_QoYvL963TwnAqSJXZLOQ') 
+plt.plot(range(nbEpisodes), stepsHistory, range(nbEpisodes), decayHistory, range(nbEpisodes), [195]*nbEpisodes)
 plt.ylabel('Number of steps')
 plt.show()
